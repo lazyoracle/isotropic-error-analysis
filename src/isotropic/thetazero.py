09f4c923ac8c7b993fadd9ab7e875ff66685b2b0
lazyoracle <contact@anuragsaharoy.me>
2025-07-27 17:39:39 +0200

    initial implementation for theta_0

--- src/isotropic/thetazero.py
+++ src/isotropic/thetazero.py
@@ -4,17 +4,27 @@ from isotropic.utils.simpsons import simpsons_rule
 import jax.numpy as jnp
 from jax import Array
 
-def get_theta_zero(d: int) -> float:
+def get_theta_zero(g:callable,) -> float:
     """generate the angle theta_0 with a normal distribution
 
     Parameters
     ----------
-    d : int
-        dimension of the system
+    g: callable
+        function g(theta) that is integrated to calculate F(theta)
 
     Returns
     -------
     float
         value of theta_0
     """
-    raise NotImplementedError("This function is not implemented yet.")
+    # We generate a random number x with a uniform distribution in the interval [0, 1].
+    x: Array = jnp.random.uniform(0, 1)
+
+    # We wrap the function g into a callable F that integrates g from 0 to theta.
+    def F(theta: float) -> Array:
+        return simpsons_rule(g, 0, theta, ..., 1e-9) # TODO: Provide the correct value for C based on the 4th derivative bound
+
+    # Use bisection to find theta_0 such that the integral equals x
+    theta_zero: float = get_theta(F, 0, jnp.pi, x, 1e-9)
+    
+    return theta_zero
