d30a06bb7f07f4c38fee82c35e8e18d4d03a71ce
lazyoracle <contact@anuragsaharoy.me>
2025-07-28 22:25:13 +0200

    tests for odd F_j

--- tests/test_e2.py
+++ tests/test_e2.py
@@ -1,7 +1,8 @@
 import jax.numpy as jnp
 from jax import Array, random
 
-from isotropic.e2 import get_e2
+from isotropic.e2 import F_j, get_e2
+from isotropic.utils.distribution import double_factorial
 
 
 def test_get_e2():
@@ -29,3 +30,55 @@ def test_get_e2():
     )
 
     assert jnp.allclose(e2, e2_expected), f"Expected {e2_expected}, got {e2}"
+
+
+def test_F_j_even():
+    """Test F_j for even j in dim d=7"""
+    d: int = 7
+    theta_j = jnp.pi / 4  # Example angle
+    j = 2
+
+    result = F_j(theta_j, j, d)
+
+    # manually calculate expected result
+    C_j = double_factorial(d - j - 1) / (jnp.pi * double_factorial(d - j - 2))
+    num = double_factorial(d - j - 2)
+    den = double_factorial(d - j - 1)
+    prefactor = C_j * num / den
+
+    # k goes from 1 to (d - j - 1) // 2, i.e., 1 to 2
+    k_val_1 = ((d - j - 2) / ((d - j - 1) * (d - j - 3))) * jnp.sin(theta_j)
+    k_val_2 = (1.0 / (d - j - 1)) * jnp.sin(theta_j) ** 3
+
+    expected_result = prefactor - (C_j * jnp.cos(theta_j) * (k_val_1 + k_val_2))
+    assert jnp.isclose(result, expected_result), (
+        f"Expected {expected_result}, got {result}"
+    )
+
+
+def test_F_j_odd():
+    """Test F_j for odd j in dim d=9"""
+    d: int = 9
+    theta_j = jnp.pi / 4  # Example angle
+    j = 3
+
+    result = F_j(theta_j, j, d)
+
+    # manually calculate expected result
+    C_j = double_factorial(d - j - 1) / (2 * jnp.pi * double_factorial(d - j - 2))
+    num = double_factorial(d - j - 2)
+    den = double_factorial(d - j - 1)
+    prefactor = C_j * num / den
+
+    # k goes from 0 to (d - j - 2) // 2, i.e., 0 to 2
+    k_val_0 = (
+        ((d - j - 2) * (d - j - 4)) / ((d - j - 1) * (d - j - 3) * (d - j - 5))
+    ) * (jnp.sin(theta_j) ** 0)
+    k_val_1 = ((d - j - 2) / ((d - j - 1) * (d - j - 3))) * (jnp.sin(theta_j) ** 2)
+    k_val_2 = (1.0 / (d - j - 1)) * (jnp.sin(theta_j) ** 4)
+    expected_result = prefactor - (
+        C_j * jnp.cos(theta_j) * (k_val_0 + k_val_1 + k_val_2)
+    )
+    assert jnp.isclose(result, expected_result), (
+        f"Expected {expected_result}, got {result}"
+    )
