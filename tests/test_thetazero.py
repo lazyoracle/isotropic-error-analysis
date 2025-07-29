d79026b69be83f31917d4f4b75e370d6764f011f
lazyoracle <contact@anuragsaharoy.me>
2025-07-29 14:18:44 +0200

    test using trapezoid

--- tests/test_thetazero.py
+++ tests/test_thetazero.py
@@ -1,8 +1,8 @@
 import jax.numpy as jnp
+from jax.scipy.integrate import trapezoid
 
 from isotropic.thetazero import get_theta_zero
 from isotropic.utils.distribution import normal_integrand
-from isotropic.utils.simpsons import simpsons_rule
 
 
 def test_get_theta_zero():
@@ -10,12 +10,14 @@ def test_get_theta_zero():
     def g(theta):
         return normal_integrand(theta, 3, 0.5)
 
-    x = 0.5  # Example value for x
+    x = 0.5  # Example value for x in [0, 1]
     theta_zero = get_theta_zero(x, g)
 
     # Check if the returned theta_zero is within the expected range
     assert 0 <= theta_zero <= jnp.pi, "theta_zero should be in [0, pi]"
 
     # Check if the integral of g from 0 to theta_zero is approximately equal to x
-    integral_value = simpsons_rule(g, 0, theta_zero, 1, 1e-9)
+    integral_value = trapezoid(
+        g(jnp.linspace(0, theta_zero, 75)), jnp.linspace(0, theta_zero, 75)
+    )
     assert jnp.isclose(integral_value, x, atol=1e-5), "Integral value does not match x"
