829938ef4cd836c2ac70a8206309d3ee347f56b5
lazyoracle <contact@anuragsaharoy.me>
2025-08-04 01:03:53 +0200

    calculate double_factorial_ratio as prod of ratio terms

--- src/isotropic/utils/distribution.py
+++ src/isotropic/utils/distribution.py
@@ -24,6 +24,53 @@ def double_factorial(n: int) -> Array:
     return jnp.where(n <= 0, 1, jnp.prod(jnp.arange(n, 0, -2, dtype=jnp.uint64)))
 
 
+def double_factorial_ratio(num: int, den: int) -> Array:
+    """
+    Computes the ratio of double factorials:
+
+        num!! / den!!
+
+    Parameters
+    ----------
+    num : int
+        The numerator for the double factorial.
+    den : int
+        The denominator for the double factorial.
+
+    Returns
+    -------
+    Array
+        The value of the ratio num!! / den!!
+
+    Notes
+    -----
+    For very large numbers, this is numerically stable only when |num - den| is ~5.
+    """
+    num_elems = jnp.arange(num, 0, -2, dtype=jnp.uint64)
+    den_elems = jnp.arange(den, 0, -2, dtype=jnp.uint64)
+
+    len_diff = den_elems.shape[0] - num_elems.shape[0]
+
+    # Ensure both num_elems and den_elems have the same length
+    if len_diff > 0:
+        num_elems = jnp.concatenate((num_elems, jnp.ones(len_diff, dtype=jnp.uint64)))
+    else:
+        den_elems = jnp.concatenate((den_elems, jnp.ones(-len_diff, dtype=jnp.uint64)))
+
+    num_len = num_elems.shape[0]
+    den_len = den_elems.shape[0]
+
+    ratio_elems = jnp.zeros(num_len // 2)
+
+    for k in jnp.arange(0, num_len // 2, 1):
+        ratio_elems = ratio_elems.at[k].set(
+            (num_elems[k] * num_elems[num_len - 1 - k])
+            / (den_elems[k] * den_elems[den_len - 1 - k])
+        )
+    ratio = jnp.prod(ratio_elems)
+    return ratio
+
+
 def normal_integrand(theta: float, d: int, sigma: float) -> Array:
     """
     Computes the function g(θ) that is integrated to calculate F(θ) which is the
